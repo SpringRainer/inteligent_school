608aac38dfba880890147abc840c386b54957ff1
Lenovo <2234668940@qq.com>
2022-03-19 16:51:22 +0800

    修复所有问题可以上线

--- src/main/java/com/example/boot_8080.java
+++ src/main/java/com/example/boot_8080.java
@@ -2,10 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class boot_8080 {
+public class boot_8080 extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(boot_8080.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(boot_8080.class);
     }
 }
